9b94ad20f303e881dd7ecfaf66b77f3453470888
EC2 Default User <ec2-user@ip-172-16-145-98.ec2.internal>
2019-04-19 05:25:55 +0000

    fixed a bug

--- container/local_test/build_and_push.sh
+++ container/local_test/build_and_push.sh
@@ -13,8 +13,8 @@ then
     exit 1
 fi
 
-chmod +x decision_trees/train
-chmod +x decision_trees/serve
+chmod +x h2oAutoEncoder/train
+chmod +x h2oAutoEncoder/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
